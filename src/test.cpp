d843a7d119156575aeee0f6cdde403f69b2cd69c
CaioRPenha <caiorpenha@gmail.com>
2025-04-05 20:19:33 +0000

    variaveis

---
+++ src/test.cpp
@@ -0,0 +1,6 @@
+#include <ionstream>
+intmain()
+{
+    int pos;
+    scanf("%d",¨& pos) if (pos < 6)
+}
---
+++ src/variaveis.cpp
@@ -0,0 +1,19 @@
+//#include <ionstream> // para o cout
+#include <stdio.h> // Para o printf
+
+int main () 
+{
+    // sintaxe printf;
+    //printf("string de formato", argumento1, argumento2, ...);
+
+    //ex1: imprimir texto simples
+    printf("Olá, mundo!\n");
+
+
+
+
+
+
+    
+    return 0;
+}
